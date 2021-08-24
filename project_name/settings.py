de2563763b6ea02c4e48da932ca51a98af483a8a
unknown <edd.edwardmike@gmail.com>
2021-08-24 15:17:35 +0100

    updated

--- project_name/settings.py
+++ project_name/settings.py
@@ -58,6 +58,7 @@ ROOT_URLCONF = 'project_name.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # or DIRS : BASE_DIR / 'templates',
         'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
